e2ff12fc1a9e4002a70bd4fce5fd838c249ffdf5
Zaphryx <lalo.gaytan@yahoo.com>
2025-02-02 18:04:27 -0700

    added .db code

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";  // Add your MySQL password
+$dbname = "ProjectDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch users
+$sql = "SELECT id, username, email FROM Users";
+$result = $conn->query($sql);
+
+$users = [];
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+}
+
+echo json_encode($users);
+$conn->close();
+?>
